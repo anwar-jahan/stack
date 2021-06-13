fd88ff5ef88026e5a060af504ab599a9825d33ef
Anwar Jahan <anwarjahan@Anwars-MacBook-Pro.local>
2021-06-13 02:32:33 -0400

    add controller

---
+++ lib/src/main/java/com/aj/HelpUtilityApplication.java
@@ -0,0 +1,14 @@
+package com.aj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpUtilityApplication.class, args);
+
+	}
+
+}
